bf10e09a7fc3ea2fce104d81aff1674acf33b3f6
김정윤 <kimjy199@naver.com>
2022-03-02 22:58:40 +0900

    update jpashop : JPA기본 실전예제2-연관관계매핑

--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -2,6 +2,7 @@ package jpabook.jpashop;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -24,15 +25,28 @@ public class JpaMain {
         // 문제가 발생할 때를 대비하는 정석 코드 try catch문
         try {
 
-            // order를 주문한 회원id 찾기
-            Order order = em.find(Order.class, 1L);
-            Long memberId = order.getMemberId(); // 식별자가 있으면 끊겨서 들어가는 느낌 => 이런 것을 객체 설계가 아닌 관계형DB 설계
+            /**엔티티 매핑 실전예제1**/
+//            // order를 주문한 회원id 찾기
+//            Order order = em.find(Order.class, 1L);
+//            Long memberId = order.getMemberId(); // 식별자가 있으면 끊겨서 들어가는 느낌 => 이런 것을 객체 설계가 아닌 관계형DB 설계
+//
+//            Member member = em.find(Member.class, memberId);
+//            // 이렇게만 보면 객체지향스럽지 않다!
+//
+//            Member findMember = order.getMember();
+//            // 객체지향스럽다!
 
-            Member member = em.find(Member.class, memberId);
-            // 이렇게만 보면 객체지향스럽지 않다!
+            /**연관관계 매핑 기초**/
+//            Order order = new Order();
+//            order.addOrderItems(new OrderItem());
 
-            Member findMember = order.getMember();
-            // 객체지향스럽다!
+            // 굳이 위와 같이 연관관계를 맺을 필요없이 아래와 같이 진행해도 된다.
+            Order order = new Order();
+            em.persist(order);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+            em.persist(orderItem);
 
             tx.commit(); // 트랜잭션 종료
         } catch (Exception e) {
--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.domain;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS")
@@ -11,10 +13,17 @@ public class Order {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+//    @Column(name = "MEMBER_ID")
+//    private Long memberId;
 
+    // 설계할 때는 단방향으로!
+    // 가급적 관계는 단방향이 좋다.
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
     private Member member; // 객체 지향스럽게!
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
 
     // 자바 8부터는 자동 매핑됨
     // spring boot에서는 카멜 스타일을 자동으로 데이터베이스 컬럼 규칙 형태(order_date)와 연결해준다.
@@ -31,20 +40,24 @@ public class Order {
         this.id = id;
     }
 
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
+//    public Long getMemberId() {
+//        return memberId;
+//    }
+//
+//    public void setMemberId(Long memberId) {
+//        this.memberId = memberId;
+//    }
 
     public Member getMember() {
         return member;
     }
 
-    public void setMember(Member member) {
+    public void changeMember(Member member) {
+        if(this.member != null) {
+            this.member.getOrders().remove(this);
+        }
         this.member = member;
+        member.getOrders().add(this);
     }
 
     public LocalDateTime getOrderDate() {
@@ -61,5 +74,18 @@ public class Order {
 
     public void setStatus(Orderstatus status) {
         this.status = status;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public void addOrderItems(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
     }
 }
